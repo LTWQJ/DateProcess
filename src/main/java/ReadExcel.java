8cd8161d57a5b12de5929d28e5865129964bbc37
ltw <3258174500@qq.com>
2023-07-24 16:19:18 +0800

    当给定的孔喉半径大于实际孔喉半径时，找到离实际孔喉半径最近的数据

--- src/main/java/ReadExcel.java
+++ src/main/java/ReadExcel.java
@@ -4,6 +4,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ReadExcel {
@@ -18,7 +20,7 @@ public class ReadExcel {
             double d =operationalData.DataOperat(1.412,0.722,0,0.06,1);
             System.out.println("测试数据x="+x+"得到结果为：y="+d);
             //创建工作簿对象
-            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream("E:\\IDEA.2022.3.2\\Workplace\\DateProcess\\src\\main\\resources\\DataList\\WC8-3N-1(1-7).xlsx"));
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream("E:\\IDEA.2022.3.2\\Workplace\\算法练习\\DateProcess\\src\\main\\resources\\DataList\\WC8-3N-1(1-5).xlsx"));
             //获取工作簿下sheet的个数
             int sheetNum = xssfWorkbook.getNumberOfSheets();
             System.out.println("该excel文件中总共有："+sheetNum+"个sheet");
@@ -51,20 +53,26 @@ public class ReadExcel {
             //todo 要将给定的孔喉半径存在数组里面
 //            声明一个数组用于存放要插值的x值（即孔喉半径）
             double [] list={0.005,0.0075,0.01 ,0.02, 0.05, 0.075, 0.1 ,0.2 ,0.5 ,0.75, 1 ,1.2 ,1.5 ,1.75 ,2, 2.2,2.5, 2.75, 3 ,3.2, 3.5 ,3.75 ,4, 4.2, 4.5 ,4.75, 5 ,5.2 ,5.5 ,5.75, 6 ,6.2 ,6.5, 6.75 ,7, 7.2, 7.5, 7.75, 8 ,8.2 ,8.5 ,8.75, 9, 9.2, 9.5, 9.75, 10 ,12, 15 ,20 ,30 ,40 ,50 ,60 ,70 ,80, 90, 100};
-                for (int n=0;n<list.length;n++){
+            double s=0;//用于接受p数组中最大的值
+            List <Double> arrayList=new ArrayList<>();//用于存大于s的所有数据
+            for (int n=0;n<list.length;n++){
                   for (int i=maxRow;i>=1;i--) {
+                      if(p[i][0]>=s){
+                          s=p[i][0];
+                      }
                     if(p[i][0]<=list[n]&&list[n]<=p[i-1][0]) {
                         double x1 = p[i][0];
                         double x2 = p[i-1][0];
                         double y1 = p[i][1];
                         double y2 = p[i-1][1];
-
-//                        System.out.println("（"+list[n] +","+ operationalData.DataOperat(x1, x2, y1, y2, list[n])+")");
                        System.out.println(list[n]);
-//                        System.out.println(operationalData.DataOperat(x1, x2, y1, y2, list[n]));
                     }
+                  }
+                  if(list[n]>s){
+                     arrayList.add(list[n]);
+                  }
                 }
-                }
+            System.out.println(Collections.min(arrayList));
             System.out.println("++++++++++++++++++++++++分析后的数据后部++++++++++++++++++++++++++++");
 //            处理数据
             //todo 要将给定的孔喉半径存在数组里面
@@ -80,6 +88,7 @@ public class ReadExcel {
                     }
                 }
             }
+            System.out.println(0);
         } catch (IOException e) {
             e.printStackTrace();
         }
